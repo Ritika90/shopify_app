f5beb58aa0d01312f0ae919b118413585ad0a460
ritikaesfera <ritika@esferasoft.com>
2017-04-25 15:10:11 +0530

    added callback url to omniauth

--- shopify_app/config/initializers/omniauth.rb
+++ shopify_app/config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
+  config.redirect_uri = "http://rt-my-shopify-app.herokuapp.com/auth/shopify/callback"
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
     scope: ShopifyApp.configuration.scope
