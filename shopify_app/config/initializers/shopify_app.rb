64c4fd49c9d8d26773f3830793ad51b12c61e023
ritikaesfera <ritika@esferasoft.com>
2017-04-25 15:01:24 +0530

    add auth

--- shopify_app/config/initializers/shopify_app.rb
+++ shopify_app/config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "eaaeb7e50d9dfa7508370cd0e7e8ad50"
   config.secret = "5e8800016a32644271fc8dd5942cc8e4"
-  # config.redirect_uri = "http://rt-my-shopify-app.herokuapp.com/auth/shopify/callback"
+  config.redirect_uri = "http://rt-my-shopify-app.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
