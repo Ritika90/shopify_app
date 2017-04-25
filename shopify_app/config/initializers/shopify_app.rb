bc0fee990334430126f196d088e3793c854caa74
ritikaesfera <ritika@esferasoft.com>
2017-04-25 14:56:52 +0530

    added keys

--- shopify_app/config/initializers/shopify_app.rb
+++ shopify_app/config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.api_key = "<api_key>"
+  config.api_key = "eaaeb7e50d9dfa7508370cd0e7e8ad50"
   config.secret = "5e8800016a32644271fc8dd5942cc8e4"
   # config.redirect_uri = "http://rt-my-shopify-app.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
