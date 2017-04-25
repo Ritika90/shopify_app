4a180aeb737cf2a37f882d87c738ac3545a3d734
ritikaesfera <ritika@esferasoft.com>
2017-04-25 13:06:56 +0530

    added secret key and changed callback url in initializers

--- shopify_app/config/initializers/shopify_app.rb
+++ shopify_app/config/initializers/shopify_app.rb
@@ -1,6 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.secret = "5e8800016a32644271fc8dd5942cc8e4"
+  config.redirect_uri = "http://rt-my-shopify-app.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
